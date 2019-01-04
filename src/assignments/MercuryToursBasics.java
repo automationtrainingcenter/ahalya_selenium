66e62c90ce8d3e0a375d6a79110e2c560b38ee13
Ahalya14 <emailme.ahalya548@gmail.com>
2019-01-04 06:39:53 +0530

    Registration_Form_Submission

--- src/assignments/MercuryToursBasics.java
+++ src/assignments/MercuryToursBasics.java
@@ -6,6 +6,59 @@ package assignments;
  * click on submit button
  */
 
-public class MercuryToursBasics {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MercuryToursBasics 
+{
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
+		WebElement registerField = driver.findElement(By.linkText("REGISTER"));
+		registerField.click();
+		WebElement firstNameField = driver.findElement(By.name("firstName"));
+		firstNameField.sendKeys("Charishma");
+		WebElement lastNameField = driver.findElement(By.name("lastName"));
+		lastNameField.sendKeys("Jasti");
+		WebElement phoneField = driver.findElement(By.name("phone"));
+		phoneField.sendKeys("9848022333");
+		WebElement emailField = driver.findElement(By.name("userName"));
+		emailField.sendKeys("cherry@123");
+		WebElement addressField = driver.findElement(By.name("address1"));
+		addressField.sendKeys("Flatno:101,Rk Towers,kukatpally");
+		WebElement cityField = driver.findElement(By.name("city"));
+		cityField.sendKeys("Hyderabad");
+		WebElement stateField = driver.findElement(By.name("state"));
+		stateField.sendKeys("Telangana");
+		WebElement postCodeField = driver.findElement(By.name("postalCode"));
+		postCodeField.sendKeys("500014");
+		WebElement userNameField = driver.findElement(By.name("email"));
+		userNameField.sendKeys("Cherry@123");
+		WebElement passwordField = driver.findElement(By.name("password"));
+		passwordField.sendKeys("Cherry1@123");
+		WebElement ConfirmPasswordField = driver.findElement(By.name("confirmPassword"));
+		ConfirmPasswordField.sendKeys("Cherry1@123");
+		WebElement submitButton = driver.findElement(By.name("register"));
+		submitButton.click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+	}
+	
 
 }
